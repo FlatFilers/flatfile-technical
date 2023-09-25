5e6a1ab6df91252dc42497e6b0976c70e83a1dd2
Britton Robitzsch <b@robitzs.ch>
2023-09-24 20:39:47 -0600

    feat!: add drag and drop using to cards on frontend
    using the react-beautiful-dnd library, added dragging and dropping cards within and between sections. (front end only)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd'
 
 import Section from './components/section'
 import { ISection } from './types/section'
@@ -23,8 +24,10 @@ export const BoardContainer = styled.div`
 
 function App() {
   const [sections, setSections] = useState<ISection[]>([])
+  // add state var to remember sections and set order of sections
 
   useEffect(() => {
+    // Refresh section order after every render
     axios.get('http://localhost:3001/sections').then((response) => {
       // Section order is determined by ID so sort by ID
       const sortedSections = response.data.sort((a: ISection, b: ISection) => a.id - b.id)
@@ -53,13 +56,87 @@ function App() {
     })
   }
 
+  // const onCardSubmit = (ISectiond: number, title: string) => {
+  //   axios({
+  //     method: 'post',
+  //     url: 'http://localhost:3001/cards',
+  //     data: { ISectiond, title }
+  //   }).then((response) => {
+  //     let sectionsClone: ISection[] = [...sections];
+  //     let targetSection = sectionsClone.find(section => section.id == ISectiond);
+
+  //     if (targetSection) {
+  //       targetSection.cards.push({
+  //         id: response.data.id,
+  //         title: response.data.title,
+  //         section_id: ISectiond
+  //       });
+  //       setSections(sectionsClone);
+  //     }
+  //   });
+  // }
+
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) return
+
+    const { source, destination } = result
+
+    if (source.droppableId === destination.droppableId) {
+      // Reorder within the same section
+      const section = sections.find((sec) => sec.id === Number(source.droppableId))
+      if (section) {
+        const reorderedCards = Array.from(section.cards)
+        const [movedCard] = reorderedCards.splice(source.index, 1)
+        reorderedCards.splice(destination.index, 0, movedCard)
+        section.cards = reorderedCards
+
+        // Update state with new card order
+        const newSections: ISection[] = sections.map((sec) =>
+          sec.id === section.id ? section : sec
+        )
+        setSections(newSections)
+
+        // Send a request to update the backend
+      }
+    } else {
+      // Move card to another section
+      const sourceSection = sections.find((sec) => sec.id === Number(source.droppableId))
+      const destinationSection = sections.find((sec) => sec.id === Number(destination.droppableId))
+
+      if (sourceSection && destinationSection) {
+        const [movedCard] = sourceSection.cards.splice(source.index, 1)
+        destinationSection.cards.splice(destination.index, 0, movedCard)
+
+        // Update state with the new card positions
+        const newSections = sections.map((sec) =>
+          sec.id === sourceSection.id
+            ? sourceSection
+            : sec.id === destinationSection.id
+            ? destinationSection
+            : sec
+        )
+        setSections(newSections)
+
+        // Send a request to update the backend
+      }
+    }
+  }
+
   return (
-    <BoardContainer>
-      {sections.map((section: ISection) => {
-        return <Section section={section} onCardSubmit={onCardSubmit}></Section>
-      })}
-    </BoardContainer>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <BoardContainer>
+        {sections.map((section: ISection) => (
+          <Droppable droppableId={String(section.id)} key={section.id}>
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                <Section section={section} onCardSubmit={onCardSubmit} />
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </BoardContainer>
+    </DragDropContext>
   )
 }
-
 export default App
